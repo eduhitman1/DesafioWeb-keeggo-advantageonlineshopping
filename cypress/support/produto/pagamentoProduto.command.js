a78e79212828530816841068244b7d3a1a26a8be
eduhitman1 <edu.hitman01.eh@gmail.com>
2024-07-28 01:21:59 -0300

    Otimização page produto pagamento

--- cypress/support/produto/pagamentoProduto.command.js
+++ cypress/support/produto/pagamentoProduto.command.js
@@ -1,11 +1,16 @@
+const BTN_ADICIONAR_KART = '#shoppingCartLink'
+const BTN_CHECKOUT = '#checkOutButton'
+
+const LAYOUT_USER_CART = '#userCart'
+
 Cypress.Commands.add('clicarBotaoCarrinho', ()=>{
-    cy.get('#shoppingCartLink').click()
+    cy.get(BTN_ADICIONAR_KART).click()
     })
     
     Cypress.Commands.add('acessarAreaCheckout', ()=>{
-    cy.get('#checkOutButton').click()
+    cy.get(BTN_CHECKOUT).click()
     })
     
     Cypress.Commands.add('validarProdutoTelaPagamento', ()=>{
-    cy.get('#userCart').should("be.visible")
+    cy.get(LAYOUT_USER_CART).should("be.visible")
     })
