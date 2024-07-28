5916f34ce240f2a31e8a122076ac5bee781fadb8
eduhitman1 <edu.hitman01.eh@gmail.com>
2024-07-28 01:06:00 -0300

    Otimização page login

--- cypress/support/login.command.js
+++ cypress/support/login.command.js
@@ -1,8 +1,15 @@
-Cypress.Commands.add('acessarAreaCadastro', ()=>{
-    cy.get('#menuUserLink').click()
-    cy.get('.create-new-account').click()
-})
+const TXT_USERNAME = '[name="username"]'
+const TXT_PASSWORD = '[name="password"]'
 
+const BTN_SIGN_IN = '#sign_in_btn'
+
+const LINK_ICON_LOGIN = '#menuUserLink'
+const LINK_CREATE_NEW_ACCOUNT = '.create-new-account'
+
+Cypress.Commands.add('acessarAreaCadastro', ()=>{
+    cy.get(LINK_ICON_LOGIN).click()
+    cy.get(LINK_CREATE_NEW_ACCOUNT).click()
+})
 
 Cypress.Commands.add('preencherUsernamePassword', ()=>{
     cy.wait(3000)
@@ -12,14 +19,12 @@ Cypress.Commands.add('preencherUsernamePassword', ()=>{
   Cypress.Commands.add('login', 
     (user = Cypress.env('login_name'),password = Cypress.env('user_password'),) => {
       const login = () =>{
-        cy.get('[name="username"]').type(user)
-        cy.get('[name="password"]').type(password, {log:false})
-      
+        cy.get(TXT_USERNAME).type(user)
+        cy.get(TXT_PASSWORD).type(password, {log:false})      
       }
       login()
 })
 
-    
 Cypress.Commands.add('botaoSignIN', ()=>{
-        cy.get('#sign_in_btn',{ timeout: 3000 }).click()
+        cy.get(BTN_SIGN_IN,{ timeout: 3000 }).click()
 })
